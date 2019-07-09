c7b7b0d58a88de869f934915a3cfaee23833c2a7
jjori <jjori.master@gmail.com>
2019-07-09 21:08:27 +0900

    vuetify 메뉴 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,38 @@
 <template>
-  <div class="container">
-    <h1>{{message}}</h1>
-  </div>
+	<v-navigation-drawer
+		v-model="drawer"
+		:mini-variant.sync="mini"
+		hide-overlay
+		stateless
+	>
+		<v-list class="pt-0" dense>
+			<v-divider></v-divider>
+
+			<v-list-tile v-for="item in items" :key="item.title">
+				<v-list-tile-action>
+					<v-icon>{{ item.icon }}</v-icon>
+				</v-list-tile-action>
+
+				<v-list-tile-content>
+					<v-list-tile-title>{{ item.title }}</v-list-tile-title>
+				</v-list-tile-content>
+			</v-list-tile>
+		</v-list>
+	</v-navigation-drawer>
 </template>
 
 <script>
 export default {
-  name: "App",
-  data() {
-    return {
-      message: "Using Parcel In A Vue.js App 2"
-    };
-  }
+	data() {
+		return {
+			drawer: true,
+			items: [
+				{ title: 'Home', icon: 'fas fa-brain 2x' },
+				{ title: 'About', icon: 'question_answer' },
+			],
+			mini: false,
+			right: null,
+		};
+	},
 };
 </script>
-
-<style scoped>
-.container {
-  width: 600px;
-  margin: 50px auto;
-  text-align: center;
-}
-</style>
